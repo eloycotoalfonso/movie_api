37d27a4220f7cc6c80c21b584f9512ec2ed98331
eloycotoalfonso <eloycoal@gmail.com>
2022-11-01 14:47:27 +0100

    Included hash procedures for passwords

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require ('mongoose');
+const { deserializeUser } = require('passport');
 
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
@@ -23,6 +24,15 @@ let userSchema = mongoose.Schema({
     birth: Date,
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
+
+userSchema.statics.hashPassword = (password) =>
+{
+    return bcrypt.hashSync(password, 10);
+}
+
+userSchema.methods.validatePassword = function (password) {
+    return bcrypt.compareSync (password, this.Password);
+}
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
